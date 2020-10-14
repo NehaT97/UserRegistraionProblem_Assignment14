69140fe306fff80db7a878132c0c711644eae71e
unknown <nehathakur764@gmail.com>
2020-10-14 11:59:47 +0530

    To check Valid Email

--- UserRegistraion.sh
+++ UserRegistraion.sh
@@ -33,9 +33,26 @@ function lastName()
         fi
 }
 
+function validEmail()
+{
+        echo "Enter the Email Id:"
+        read emailid
+
+        match="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
+
+        if [[ $emailid =~ $match ]]
+        then
+                echo "valid emailid"
+        else
+                echo "Invalid emailid"
+
+        fi
+}
+
 main()
 {
-firstName
-lastName
+#firstName
+#lastName
+validEmail
 }
 main
