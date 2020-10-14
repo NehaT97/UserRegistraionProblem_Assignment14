a82de2371b86d119ca0daf57f98ebf3df9bdeba3
unknown <nehathakur764@gmail.com>
2020-10-14 11:30:25 +0530

    Check Valid First Name

--- UserRegistraion.sh
+++ UserRegistraion.sh
@@ -1,1 +1,25 @@
+#!/bin/bash -x
 #Welcome to User Registraion System#
+
+function firstName()
+{
+	echo "Enter the first name:"
+	read firstname
+
+	match="^[[:upper:]][a-z]{2,}"
+
+	if [[ $firstname =~ $match ]]
+	then 
+		echo "valid firstname"
+	else 
+		echo "Invalid firstname"
+
+	fi
+}
+
+
+main()
+{
+firstName
+}
+main
